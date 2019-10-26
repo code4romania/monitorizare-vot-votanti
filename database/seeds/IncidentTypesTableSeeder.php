2511532b77969b3c24ed98b017a315936591aaef
Andrei Ciuculescu <ciuculescu.andrei.kappa@gmail.com>
2019-10-26 14:38:54 +0300

    issue 43: fix according to comments
    * added camel case
    * created a variable to hold incident types
    * modified both seeders to use this variable as a data source

--- database/seeds/IncidentTypesTableSeeder.php
+++ database/seeds/IncidentTypesTableSeeder.php
@@ -1,9 +1,72 @@
 <?php
+
 use Illuminate\Database\Seeder;
 use App\IncidentType;
 
 class IncidentTypesTableSeeder extends Seeder
 {
+    public $incidentTypes = [
+        [
+            'id' => 1,
+            'label' => 'IT_OTHER',
+            'code' => 'OTH',
+            'name' => 'Altul'
+        ],
+        [
+            'id' => 2,
+            'label' => 'IT_OBSERVERS',
+            'code' => 'ELE',
+            'name' => 'Probleme legate de campania electorală'
+        ],
+        [
+            'id' => 3,
+            'label' => 'IT_MEDIA',
+            'code' => 'MED',
+            'name' => 'Discurs instigator la ură'
+        ],
+        [
+            'id' => 4,
+            'label' => 'IT_BRIBE',
+            'code' => 'MIT',
+            'name' => 'Vot multiplu/Mită electorală'
+        ],
+        [
+            'id' => 5,
+            'label' => 'BUILDING',
+            'code' => 'NBE',
+            'name' => 'Nereguli legate de funcționarea administrației electorale'
+        ],
+        [
+            'id' => 7,
+            'label' => 'LOCATION',
+            'code' => 'OBP',
+            'name' => 'Vot în străinătate'
+        ],
+        [
+            'id' => 8,
+            'label' => 'IT_ELEC_TURISM',
+            'code' => 'TEL',
+            'name' => 'Transport organizat de alegători'
+        ],
+        [
+            'id' => 9,
+            'label' => 'IT_PUBLIC_FOUNDS',
+            'code' => 'FEL',
+            'name' => 'Abuz de resurse publice'
+        ],
+        [
+            'id' => 11,
+            'label' => 'POLICE',
+            'code' => 'OPN',
+            'name' => 'Presiuni din partea autorităților locale'
+        ],
+        [
+            'id' => 12,
+            'label' => 'VOTE',
+            'code' => 'NUM',
+            'name' => 'Probleme în zilele votului'
+        ]
+    ];
 
     /**
      * Run the database seeds.
@@ -12,65 +75,9 @@ class IncidentTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        IncidentType::create([
-            'id' => 1,
-            'label' => 'IT_OTHER',
-            'code' => 'OTH',
-            'name' => 'Altul'
-        ]);
-        IncidentType::create([
-            'id' => 2,
-            'label' => 'IT_OBSERVERS',
-            'code' => 'ELE',
-            'name' => 'Probleme legate de campania electorală'
-        ]);
-        IncidentType::create([
-            'id' => 3,
-            'label' => 'IT_MEDIA',
-            'code' => 'MED',
-            'name' => 'Discurs instigator la ură'
-        ]);
-        IncidentType::create([
-            'id' => 4,
-            'label' => 'IT_BRIBE',
-            'code' => 'MIT',
-            'name' => 'Vot multiplu/Mită electorală'
-        ]);
-        IncidentType::create([
-            'id' => 5,
-            'label' => 'BUILDING',
-            'code' => 'NBE',
-            'name' => 'Nereguli legate de funcționarea administrației electorale'
-        ]);
-        IncidentType::create([
-            'id' => 7,
-            'label' => 'LOCATION',
-            'code' => 'OBP',
-            'name' => 'Vot în străinătate'
-        ]);
-        IncidentType::create([
-            'id' => 8,
-            'label' => 'IT_ELEC_TURISM',
-            'code' => 'TEL',
-            'name' => 'Transport organizat de alegători'
-        ]);
-        IncidentType::create([
-            'id' => 9,
-            'label' => 'IT_PUBLIC_FOUNDS',
-            'code' => 'FEL',
-            'name' => 'Abuz de resurse publice'
-        ]);
-        IncidentType::create([
-            'id' => 11,
-            'label' => 'POLICE',
-            'code' => 'OPN',
-            'name' => 'Presiuni din partea autorităților locale'
-        ]);
-        IncidentType::create([
-            'id' => 12,
-            'label' => 'VOTE',
-            'code' => 'NUM',
-            'name' => 'Probleme în zilele votului'
-        ]);
+        foreach ($this->incidentTypes as $incidentType) {
+            IncidentType::create($incidentType);
+        }
     }
+
 }
--- database/seeds/IncidentsTableSeeder.php
+++ database/seeds/IncidentsTableSeeder.php
@@ -15,27 +15,30 @@ class IncidentsTableSeeder extends Seeder
     {
 
         $faker = Faker::create();
+        $incidentTypeIds = $this->getIncidentIds();
 
-        /*
-         * Follow the convention of IncidentTypesTableSeeder
-         * and only pass the following values for the var
-         * incident_type_id when seeding the local db
-         */
-        $incident_type_ids = [1,2,3,4,5,7,8,9,11,12];
-
-        foreach (range(1, 141) as $index)
-        {
+        foreach (range(1, 141) as $index) {
             Incident::create([
                 'first_name' => $faker->firstName(),
                 'last_name' => $faker->lastName(),
                 'county_id' => $faker->numberBetween(1, 42),
                 'city_id' => $faker->numberBetween(1, 3000),
                 'precinct_id' => $faker->numberBetween(1, 90000),
-                'incident_type_id' => $incident_type_ids[array_rand($incident_type_ids)],
+                'incident_type_id' => $incidentTypeIds[array_rand($incidentTypeIds)],
                 'description' => $faker->realText(250),
                 'image_url' => $faker->imageUrl(320, 240, 'cats'),
                 'status' => $faker->randomElement(['Approved', 'Pending', 'Rejected'])
             ]);
         }
+    }
+
+    /**
+     * @return array
+     */
+    private function getIncidentIds()
+    {
+        return array_map(function ($incidentType) {
+            return $incidentType['id'];
+        }, (new IncidentTypesTableSeeder())->incidentTypes);
     }
 }
